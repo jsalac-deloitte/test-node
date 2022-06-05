3198824a60811382552ed1cfb6f9c63c6e008a0c
Joseph Salac <jsalac@deloitte.com>
2022-06-05 22:32:19 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,64 +1,15 @@
-/* eslint no-console: 0*/
-const express = require("express");
-
-const nodemailer = require("nodemailer");
-const nodemailerSendgrid = require("nodemailer-sendgrid"); //require('../index');
-
-const router = express.Router();
-const app = express();
+var express = require("express");
 var cors = require("cors");
-app.use(cors());
+var app = express();
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://maddogcarclub.io");
-//   res.setHeader("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   next();
-// });
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/", router);
-app.listen(PORT, () => console.log("Server Running on port " + PORT));
-
-// const transport = nodemailer.createTransport(
-//   nodemailerSendgrid({
-//     apiKey:
-//       "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",
-//   })
-// );
-
-router.get("/", function (req, res) {
-  res.send("this is running from heruko with port " + PORT);
+app.get("/", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins! " + new Date() });
 });
 
-// router.post("/", (req, res) => {
-//   const pos1 = req.body.pos1;
-//   const name = req.body.name;
-//   const email = req.body.email;
-//   const linkedin = req.body.linkedin;
-//   const portfolio = req.body.portfolio;
-//   const my_file = req.body.my_file;
-//   const mail = {
-//     from: "Mad Dog Car Club <willhelmet042002@gmail.com>",
-//     to: "willhelmet042002@gmail.com",
-//     subject: `Applicant for ${pos1}`,
-//     html: `<p>Name: ${name}</p>
-//              <p>Email: ${email}</p>
-//              <p>Linkedin: ${linkedin}</p>
-//              <p>Portfolio: ${portfolio}</p>`,
-//     attachments: [
-//       {
-//         filename: `${my_file}`,
-//         path: `${__dirname}/attachment.docx`,
-//       },
-//     ],
-//   };
-//   transport.sendMail(mail, (error) => {
-//     if (error) {
-//       res.json({ status: "ERROR" });
-//     } else {
-//       res.json({ status: "Message Sent" });
-//     }
-//   });
-// });
+app.listen(PORT, function () {
+  console.log("CORS-enabled web server listening on port 80");
+});
