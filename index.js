f01a3863b7a7d4317e0eccf7d312ea1bf7a39f94
Joseph Salac <jsalac@deloitte.com>
2022-06-05 22:20:14 +0800

    added end and procfile

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var app = express();
 
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 app.get("/", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins! " + new Date() });
 });
 
-app.listen(80, function () {
+app.listen(PORT, function () {
   console.log("CORS-enabled web server listening on port 80");
 });
